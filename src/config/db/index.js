fb61ea329ed68d5ea0005e077e91af90e04b22d2
doantanty92 <doantanty92@gmail.com>
2022-07-29 01:36:54 +0700

    add cloud mongodb

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 async function connect() {
   try {
-    await mongoose.connect('mongodb://localhost:27017/f8_education_dev')
+    await mongoose.connect('mongodb+srv://snake-demo:snake123123@snakef8.9wmiu.mongodb.net/?retryWrites=true&w=majority')
+   
     console.log('Connect successfully')
   } catch (error) {
     console.log('Connect failed: ' + error)
